15550fb35b59116aee53c2d2a4afeffc1a829263
My Name <foo@lisp.us.to>
2017-08-25 03:59:23 -0500

    Large changes to work with new syscalls

--- sigma0.c
+++ sigma0.c
@@ -9,24 +9,36 @@
 #define DBG_PRINTF( FORMAT, ... ) \
 	c4_debug_printf( "--- sigma0: " FORMAT, __VA_ARGS__ )
 
+typedef struct c4_process {
+	int addrspace;
+	int capspace;
+	int endpoint;
+	int thread;
+	bool running;
+} c4_process_t;
+
 struct foo {
 	int target;
 	int display;
-	int nameserver;
+	c4_process_t nameserver;
 };
 
 extern char initfs_start[];
 extern char initfs_end[];
 
 static tar_header_t *tar_initfs = (void *)initfs_start;
+// TODO: make a tree of available pages
+int phys_page_buffer = -1;
 
-int elf_load( Elf32_Ehdr *elf, int nameserver );
-int elf_load_file( const char *name, int nameserver );
-int elf_load_tar_header( tar_header_t *header, int nameserver );
+c4_process_t elf_load( Elf32_Ehdr *elf, int nameserver );
+c4_process_t elf_load_file( const char *name, int nameserver );
+c4_process_t elf_load_tar_header( tar_header_t *header, int nameserver );
 
 static void  bss_init( void );
-static void *allot_pages( unsigned pages );
+//static void *allot_pages( unsigned pages );
+static int allot_pages( unsigned pages );
 static void  server( void *data );
+static void  test_thread( void );
 
 void main( void ){
 	struct foo thing;
@@ -34,8 +46,41 @@ void main( void ){
 	// set the memory in bss to zero, which is needed since this loaded
 	// as a flat binary
 	bss_init( );
+	int ret = 0;
+	message_t msg;
+
+	do {
+		c4_msg_recieve( &msg, 1 );
+
+		switch ( msg.type ){
+			case MESSAGE_TYPE_GRANT_OBJECT:
+				{
+					uint32_t frame = msg.data[5];
+					c4_debug_printf( "--- sigma0: recieved a capability "
+					                 "at %u, type: %u\n",
+					                 msg.data[5], msg.data[0] );
+
+					if ( phys_page_buffer < 0 ){
+						phys_page_buffer = frame;
+					}
+				}
+				break;
+
+			default:
+				c4_debug_printf( "--- got some other crap: %u\n", msg.type );
+				break;
+		}
+
+	} while ( msg.type != 1234 );
 
 	thing.nameserver = elf_load_file( "./bin/nameserver", 0 );
+
+	msg = (message_t){
+		.type = 12345,
+		.data = {0},
+	};
+
+	c4_debug_printf( "--- sigma0: exiting...\n" );
 
 	for ( tar_header_t *iter = tar_initfs;
 	      !tar_end(iter);
@@ -46,9 +91,9 @@ void main( void ){
 			continue;
 		}
 
-		int id = elf_load_tar_header( iter, thing.nameserver );
+		elf_load_tar_header( iter, thing.nameserver.endpoint );
 		c4_debug_printf( "--- sigma0: started \"%s\" at id %u\n",
-				iter->filename, id );
+				iter->filename, 0 );
 	}
 
 	server( &thing );
@@ -56,6 +101,21 @@ void main( void ){
 	// TODO: panic or dump debug info or something, server()
 	//       should never return
 	for ( ;; );
+}
+
+static void test_thread( void ){
+	message_t msg = {
+		.type = 123456,
+		.data = {1, 2, 3, 4, 5},
+	};
+
+	*(volatile unsigned *)0 = 1234;
+
+	c4_msg_send( &msg, 1 );
+	//DBG_PRINTF( "got here man, %u\n", 123 );
+	c4_debug_printf( "--- sigma0: testing this\n" );
+	//for (;;);
+	c4_exit();
 }
 
 static inline bool is_page_fault( const message_t *msg ){
@@ -84,7 +144,7 @@ static void server( void *data ){
 	message_t msg;
 
 	while ( true ){
-		c4_msg_recieve( &msg, 0 );
+		c4_msg_recieve( &msg, 1 );
 
 		if ( is_bootinfo_fault( &msg )){
 			DBG_PRINTF( "bootinfo memory request from %u\n", msg.sender );
@@ -100,7 +160,7 @@ static void server( void *data ){
 				msg.data[0], msg.data[1]
 			);
 
-			c4_dump_maps( msg.sender );
+			//c4_dump_maps( msg.sender );
 
 		} else if ( is_page_request( &msg )){
 			DBG_PRINTF( "request for %u pages at %p\n",
@@ -109,7 +169,8 @@ static void server( void *data ){
 			// TODO: two things, keep track of available memory, and check that
 			//       there is actually enough memory to satisfy the request
 			unsigned pages = msg.data[2];
-			void *page = allot_pages( pages );
+			//void *page = allot_pages( pages );
+			void *page = NULL;
 			void *addr = (void *)msg.data[0];
 			unsigned permissions = msg.data[1];
 			unsigned long sender = msg.sender;
@@ -133,12 +194,9 @@ static void bss_init( void ){
 	}
 }
 
-static void *allot_pages( unsigned pages ){
-	static uint8_t *s = (void *)0xd0001000;
-	void *ret = s;
-
-	s += pages * PAGE_SIZE;
-
+static int allot_pages( unsigned pages ){
+	int ret = phys_page_buffer;
+	phys_page_buffer = c4_phys_frame_split( phys_page_buffer, pages );
 	return ret;
 }
 
@@ -150,51 +208,89 @@ static inline void elf_load_set_arg( uint8_t *stack,
 	*((unsigned *)(stack + offset) + arg + 1) = value;
 }
 
-int elf_load( Elf32_Ehdr *elf, int nameserver ){
+#define BUGOUT(X) \
+	if ( (X) < 0 ){\
+		c4_debug_printf("--- sigma0: ERROR: %u: %u\n", __LINE__, -(X));\
+	}
+
+c4_process_t elf_load( Elf32_Ehdr *elf, int nameserver ){
+	c4_process_t ret;
+
 	unsigned stack_offset = 0xff8;
+	int frame = allot_pages(1);
+	int cspace = c4_cspace_create();
+	int aspace = c4_addrspace_create();
 
-	void *entry      = (void *)elf->e_entry;
-	void *to_stack   = (uint8_t *)0xa0000000;
-	void *from_stack = (uint8_t *)allot_pages(1);
-	void *stack      = (uint8_t *)to_stack + stack_offset;
+	void *entry          = (void *)elf->e_entry;
+	uintptr_t to_stack   = 0xa0000000;
+	uintptr_t from_stack = 0xda000000;
+	uintptr_t stack      = to_stack + stack_offset;
 
-	// copy the output info to the new stack
-	elf_load_set_arg( from_stack, stack_offset, 0, nameserver );
+	int c4ret = 0;
 
-	int thread_id = c4_create_thread( entry, stack,
-	                                  THREAD_CREATE_FLAG_NEWMAP);
+	int thread_id = c4_create_thread( entry, (void *)stack, 0 );
+	c4_debug_printf( "--- sigma0: made thread %u\n", thread_id );
+	BUGOUT(thread_id);
+	c4_set_addrspace( thread_id, aspace );
+	c4_set_capspace( thread_id, cspace );
+	c4_set_pager( thread_id, 1 );
 
-	c4_mem_grant_to( thread_id, from_stack, to_stack, 1,
-	                 PAGE_READ | PAGE_WRITE );
+	int msgq = c4_msg_create_sync();
+	c4_cspace_copy( 0, msgq,       cspace, 1 );
+	c4_cspace_copy( 0, nameserver, cspace, 2 );
+	c4_cspace_restrict( cspace, nameserver,
+	                    CAP_MODIFY | CAP_SHARE | CAP_MULTI_USE );
+
+	ret.addrspace = aspace;
+	ret.capspace  = cspace;
+	ret.thread    = thread_id;
+	ret.endpoint  = msgq;
+
+	c4ret = c4_addrspace_map( 2, frame, from_stack, PAGE_READ | PAGE_WRITE );
+	BUGOUT(c4ret);
+	c4ret = c4_addrspace_map( aspace, frame, to_stack, PAGE_READ | PAGE_WRITE );
+	BUGOUT(c4ret);
+	elf_load_set_arg( (void *)from_stack, stack_offset, 0, 2 );
+	c4ret = c4_addrspace_unmap( 2, from_stack );
+	BUGOUT(c4ret);
 
 	// load program headers
 	for ( unsigned i = 0; i < elf->e_phnum; i++ ){
 		Elf32_Phdr *header = elf_get_phdr( elf, i );
 		uint8_t *progdata  = (uint8_t *)elf + header->p_offset;
-		void    *addr      = (void *)header->p_vaddr;
+		uintptr_t addr     = header->p_vaddr;
 		unsigned pages     = (header->p_memsz / PAGE_SIZE)
 		                   + (header->p_memsz % PAGE_SIZE > 0);
-		uint8_t *databuf   = allot_pages( pages );
+		int frame          = allot_pages( pages );
+		uintptr_t databuf  = 0xda000000;
+		uint8_t *dataptr   = (void *)databuf;
 		unsigned offset    = header->p_vaddr % PAGE_SIZE;
-		void    *adjaddr   = (uint8_t *)addr - offset;
-
-		for ( unsigned k = 0; k < header->p_filesz; k++ ){
-			databuf[k + offset] = progdata[k];
-		}
+		uintptr_t adjaddr  = addr - offset;
 
 		// TODO: translate elf permissions into message permissions
+		c4_addrspace_map( 2,      frame, databuf, PAGE_READ | PAGE_WRITE );
+		c4_addrspace_map( aspace, frame, adjaddr, PAGE_READ | PAGE_WRITE );
+
+		for ( unsigned k = 0; k < header->p_filesz; k++ ){
+			dataptr[k + offset] = progdata[k];
+		}
+
+		c4_addrspace_unmap( 2, databuf );
+		c4_cspace_move( 0, frame, cspace, i + 3 );
+
+		/*
 		c4_mem_grant_to( thread_id, databuf, adjaddr, pages,
 		                 PAGE_READ | PAGE_WRITE );
+		 */
 	}
 
-	c4_set_pager( thread_id, 1 );
 	c4_continue_thread( thread_id );
-
-	return thread_id;
+	return ret;
 }
 
-int elf_load_file( const char *name, int nameserver ){
-	int ret = -1;
+c4_process_t elf_load_file( const char *name, int nameserver ){
+	//c4_process_t ret = -1;
+	c4_process_t ret;
 	tar_header_t *lookup = tar_lookup( tar_initfs, name );
 
 	if ( lookup ){
@@ -206,8 +302,8 @@ int elf_load_file( const char *name, int nameserver ){
 	return ret;
 }
 
-int elf_load_tar_header( tar_header_t *header, int nameserver ){
-	int ret = -1;
+c4_process_t elf_load_tar_header( tar_header_t *header, int nameserver ){
+	c4_process_t ret;
 
 	if ( header ){
 		void *data = tar_data( header );
